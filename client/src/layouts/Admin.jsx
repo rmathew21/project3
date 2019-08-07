5d5cbfb51eafacbb137ed3ad6fa3208d3575f027
rob <robmathew21@gmail.com>
2019-08-06 19:50:03 -0500

    fix#2

--- client/src/layouts/Admin.jsx
+++ client/src/layouts/Admin.jsx
@@ -116,7 +116,7 @@ class Dashboard extends React.Component {
       <div className={classes.wrapper}>
         <Sidebar
           routes={routes}
-          logoText={"Creative Tim"}
+          logoText={"Educated Trader"}
           logo={logo}
           image={this.state.image}
           handleDrawerToggle={this.handleDrawerToggle}
